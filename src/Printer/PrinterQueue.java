8497e81e3f0af5da85e29414058197954c8bd239
VibeeshST <vibeesh.cd22@bitsathy.ac.in>
2026-03-30 20:17:53 +0530

    commited again

--- src/Printer/PrinterQueue.java
+++ src/Printer/PrinterQueue.java
@@ -1,4 +1,75 @@
 package Printer;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class PrinterQueue {
+    Scanner in = new Scanner(System.in);
+
+    public PrinterQueue(){
+
+    };
+
+    Queue<PrintJob> queue = new LinkedList<>();
+
+    public void executePrint() throws InterruptedException {
+        PrintJob job =  queue.poll();
+        if(job!=null){
+            System.out.println("Printing: "+ job);
+            Thread.sleep(job.pages * 500);
+            System.out.println("Finished printing: "+ job.name);
+        }
+        else {
+            System.out.println("No jobs left");
+        }
+    }
+
+
+    public void currentPrint(){
+        PrintJob job = queue.peek();
+        if(job != null) {
+            System.out.println("Current print :" + job);
+        }
+        else{
+            System.out.println("No job in the queue");
+        }
+    }
+
+    public void addJob() {
+        System.out.println("Enter job name");
+        String name = in.nextLine();
+
+        System.out.println("Enter number of pages");
+        int pages = in.nextInt();
+        in.nextLine();
+
+        if (name != null && !name.isEmpty() && pages > 0) {
+            queue.offer(new PrintJob(name, pages));
+            System.out.println("Added job: " + name);
+        } else {
+            System.out.println("Invalid job details");
+        }
+    }
+     public void removeJob () {
+
+            PrintJob job = queue.poll();
+            if (job != null) {
+                System.out.println("Removed job: " + job);
+            } else {
+                System.out.println("Queue is Empty");
+            }
+    }
+
+        public void waitingQueue () {
+         if(!queue.isEmpty()){
+             System.out.println("All the jobs left: "+ queue);
+         }
+         else {
+             System.out.println("No jobs left");
+         }
+        }
+
+
 }
+
--- src/Printer/PrinterRun.java
+++ src/Printer/PrinterRun.java
@@ -1,4 +1,52 @@
 package Printer;
 
+import java.util.Scanner;
+
 public class PrinterRun {
+
+    Scanner in =  new Scanner(System.in);
+    int choice;
+    PrinterQueue queue = new PrinterQueue();
+public void runMenu() {
+    while (true) {
+        System.out.println("\nPrinter Menu:");
+        System.out.println(("1. Show waiting queue"));
+        System.out.println("2. Add Job");
+        System.out.println("3. Remove Job");
+        System.out.println("4. Execute Print");
+        System.out.println("5. Current Print");
+        System.out.println("6. Exit");
+        System.out.println("Enter your choice");
+
+        choice = in.nextInt();
+        in.nextLine();
+        switch (choice) {
+            case 1:
+                queue.waitingQueue();
+                break;
+            case 2:
+                queue.addJob();
+                break;
+            case 3:
+                queue.removeJob();
+                break;
+            case 4:
+                try {
+                    queue.executePrint();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            case 5:
+                queue.currentPrint();
+                break;
+            case 6:
+                System.out.println("Exiting printer program.");
+                return;
+            default:
+                System.out.println("Invalid Choice;");
+        }
+    }
 }
+}
+
